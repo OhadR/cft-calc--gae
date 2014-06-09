4f00217d2e9a8f377165da8d2526ff52aeabbfd9
OhadR <ohadr.developer@gmail.com>
2014-06-09 15:26:38 +0300

    test

--- cft_calc/src/test/java/com/ohadr/cftcalc/tests/InMemRepository.java
+++ cft_calc/src/test/java/com/ohadr/cftcalc/tests/InMemRepository.java
@@ -1,19 +1,17 @@
 package com.ohadr.cftcalc.tests;
 
+import java.awt.Window;
 import java.util.*;
 
-import com.ohadr.cbenchmarkr.Trainee;
-import com.ohadr.cbenchmarkr.Workout;
-import com.ohadr.cbenchmarkr.interfaces.IPerson;
-import com.ohadr.cbenchmarkr.interfaces.IRepository;
+import com.ohadr.cftcalc.CftCalcConstants;
+import com.ohadr.cftcalc.Person;
+import com.ohadr.cftcalc.Workout;
+import com.ohadr.cftcalc.interfaces.IPerson;
+import com.ohadr.cftcalc.interfaces.IRepository;
 
 public class InMemRepository implements IRepository
 {
-	private static final String ANNIE = "annie";
-	private static final String BARBARA = "barbara";
-	private static final String CINDY = "cindy";
-
-	private Map<String, IPerson> persons = new HashMap<String, IPerson>();
+	private List<IPerson> persons = new ArrayList<IPerson>();
 
 	private Set<String> workoutsNames = new HashSet<String>();
 
@@ -24,25 +22,11 @@ public class InMemRepository implements IRepository
 	
 	private void init()
 	{
-		IPerson personA = new Trainee("moshe");
-		personA.addWorkout(new Workout(CINDY, 100));
-		personA.addWorkout(new Workout(CINDY, 120));
-		persons.put(personA.getId(), personA);
-		
-		IPerson personB = new Trainee("kookoo");
-		personB.addWorkout(new Workout(CINDY, 100));
-		personB.addWorkout(new Workout(BARBARA, 60));
-		persons.put(personB.getId(), personB);
-		
-		
-		workoutsNames.add(CINDY);
-		workoutsNames.add(BARBARA);
-		workoutsNames.add(ANNIE);
 	}
 
-	public Collection<IPerson> getAllTrainees()
+	public List<IPerson> getAllPersons()
 	{
-		return persons.values();
+		return persons;
 	}
 
 	public List<Workout> getAllWorkouts()
@@ -56,23 +40,14 @@ public class InMemRepository implements IRepository
 		return workoutsNames;
 	}
 
-	@Override
-	public void addPerson(IPerson personA)
+	public void addPerson(IPerson person)
 	{
-		// TODO Auto-generated method stub
-		
+		persons.add( person );
 	}
 
-	@Override
-	public void addWorkoutForTrainee(String trainee, Workout workout)
+	public void addWorkout(String workout)
 	{
-		IPerson traineeEntry = persons.get( trainee );
-		if( traineeEntry == null )
-		{
-			//error; this should never happen.
-			throw new RuntimeException();			
-		}
-		traineeEntry.addWorkout( workout );
+		workoutsNames.add( workout );
 	}
 
 }
