7cd266c881fb84ff9c0a29bfc182745b4257412f
OhadR <ohadr.developer@gmail.com>
2014-07-30 23:53:17 +0300

    minor

--- cft_calc/src/main/java/com/ohadr/cbenchmarkr/core/RepositoryCacheImpl.java
+++ cft_calc/src/main/java/com/ohadr/cbenchmarkr/core/RepositoryCacheImpl.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
